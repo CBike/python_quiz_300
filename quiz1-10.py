1fb8cf2f586b7f65ea3d001610883864bd9bde00
yangho.jeon <64768242+CBike@users.noreply.github.com>
2022-01-03 23:37:59 +0900

    파이썬 300제 0~10번문제풀이

---
+++ quiz1-10.py
@@ -0,0 +1,42 @@
+# TODO  파이썬 300제 001 ~ 010
+
+
+# 001
+print("Hello World")
+
+# 002
+print("Mary`s cosmetics")
+
+# 003
+print('신 씨가 소리 질럿다 "도둑이야." ')
+
+# 004
+
+print('"C:\\windows"')
+
+# 005
+
+print("안녕하세요. \n 만나서 \t\t 반갑습니다.")
+# answer :: \n : 줄바꿈 , \t 탭 처리
+
+# 006
+# 오늘은 , 일요일
+
+# 007
+
+print('naver;kakao;sk;samsung;')
+# print 함수의 sep 인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론이 출력됩니다.
+
+print("naver", "kakao", "samsung", sep=";")
+
+# 008
+
+print("naver", "kakao", "samsung","sk", sep="\\")
+
+# 009
+
+print("first", end=""); print("second")
+
+# 010
+
+print(5/3)
